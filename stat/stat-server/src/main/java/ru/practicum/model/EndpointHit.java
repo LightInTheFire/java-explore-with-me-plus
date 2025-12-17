020c780d51fa676069347a2d26895a5d137f77d0
Карина <karinkademchenko@yandex.ru>
2025-12-17 16:04:37 +0300

    Добавление сущности EndpointHit

---
+++ stat/stat-server/src/main/java/ru/practicum/model/EndpointHit.java
@@ -0,0 +1,32 @@
+package ru.practicum.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "stat")
+public class EndpointHit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(name = "app")
+    String app;
+
+    @Column(name = "uri")
+    String uri;
+
+    @Column(name = "ip")
+    String ip;
+
+    @Column(name = "timestamp")
+    LocalDateTime created;
+}
