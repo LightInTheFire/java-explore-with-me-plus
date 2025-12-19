923145023158c130779f14340e8da93ed81764c3
basarus51 <basarus51@gmail.com>
2025-12-19 23:33:48 +0300

    - Добавлен клиент stat_svc - Добавлены тесты клиента stat_svc - В schema.sql исправлен ip под IPv6

---
+++ stat/stat-client/src/main/java/ru/practicum/dto/EndpointHitDto.java
@@ -0,0 +1,20 @@
+package ru.practicum.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class EndpointHitDto {
+    private String app;
+    private String uri;
+    private String ip;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime timestamp;
+}
