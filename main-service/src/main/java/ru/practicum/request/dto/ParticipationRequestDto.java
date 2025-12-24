703c4366581ae07e1b40771c26700fec4efc5928
Ilia Egorov <aleilmessia@mail.ru>
2025-12-24 13:59:11 +0300

    feat: add all dto's from spec

---
+++ main-service/src/main/java/ru/practicum/request/dto/ParticipationRequestDto.java
@@ -0,0 +1,6 @@
+package ru.practicum.request.dto;
+
+import java.time.LocalDateTime;
+
+public record ParticipationRequestDto(
+        LocalDateTime created, Long event, Long id, Long requester, String status) {}
