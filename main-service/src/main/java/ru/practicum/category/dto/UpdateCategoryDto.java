818065f0ac30de01fd76427cd690eb9cfab15e6f
Карина <karinkademchenko@yandex.ru>
2025-12-26 17:06:19 +0300

    Add UpdateCategoryDto

---
+++ main-service/src/main/java/ru/practicum/category/dto/UpdateCategoryDto.java
@@ -0,0 +1,6 @@
+package ru.practicum.category.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record UpdateCategoryDto(@Size(min = 1, max = 50) @NotBlank String name) {}
