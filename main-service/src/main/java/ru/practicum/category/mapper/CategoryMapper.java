f8e753e2d9ea68b61799fb4262b3dec3ea77ef87
Карина <karinkademchenko@yandex.ru>
2025-12-26 17:05:53 +0300

    Add CategoryMapper

---
+++ main-service/src/main/java/ru/practicum/category/mapper/CategoryMapper.java
@@ -0,0 +1,15 @@
+package ru.practicum.category.mapper;
+
+import ru.practicum.category.dto.CategoryDto;
+import ru.practicum.category.dto.NewCategoryDto;
+import ru.practicum.category.model.Category;
+
+public class CategoryMapper {
+    public static Category mapToEntity(NewCategoryDto newCategoryDto) {
+        return new Category(null, newCategoryDto.name());
+    }
+
+    public static CategoryDto mapToDto(Category category) {
+        return new CategoryDto(category.getId(), category.getName());
+    }
+}
