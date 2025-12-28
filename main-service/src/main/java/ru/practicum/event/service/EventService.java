0e957677c9d579362beb4f9edbf4ab9f6c0cc923
Ilia Egorov <aleilmessia@mail.ru>
2025-12-28 14:22:36 +0300

    feat: add methods to event service interface

--- main-service/src/main/java/ru/practicum/event/service/EventService.java
+++ main-service/src/main/java/ru/practicum/event/service/EventService.java
@@ -2,16 +2,23 @@ package ru.practicum.event.service;
 
 import java.util.Collection;
 
-import ru.practicum.event.dto.EventFullDto;
-import ru.practicum.event.dto.EventShortDto;
-import ru.practicum.event.dto.UpdateEventAdminRequest;
+import ru.practicum.event.dto.*;
 
 public interface EventService {
     EventFullDto getById(Long eventId);
 
-    Collection<EventShortDto> getEvents(EventsPublicGetRequest request);
+    Collection<EventShortDto> getEvents(EventsPublicGetRequest getRequest);
 
     EventFullDto updateEvent(Long eventId, UpdateEventAdminRequest updateRequest);
 
     Collection<EventFullDto> getEvents(EventsAdminGetRequest getRequest);
+
+    Collection<EventShortDto> getEvents(EventsPrivateGetRequest getRequest);
+
+    EventFullDto createEvent(Long userId, NewEventDto newEventDto);
+
+    EventFullDto getByUserById(Long userId, Long eventId);
+
+    EventFullDto updateEventByUserById(
+            Long userId, Long eventId, UpdateEventUserRequest updateRequest);
 }
