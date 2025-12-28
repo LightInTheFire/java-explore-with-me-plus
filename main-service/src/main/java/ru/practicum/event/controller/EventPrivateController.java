f29732a07b9b5027924694b1a7f4367aa2577851
Ilia Egorov <aleilmessia@mail.ru>
2025-12-28 14:21:41 +0300

    feat: update event private controller

--- main-service/src/main/java/ru/practicum/event/controller/EventPrivateController.java
+++ main-service/src/main/java/ru/practicum/event/controller/EventPrivateController.java
@@ -1,7 +1,18 @@
 package ru.practicum.event.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.Collection;
+
+import jakarta.validation.Valid;
+
+import ru.practicum.event.dto.EventFullDto;
+import ru.practicum.event.dto.EventShortDto;
+import ru.practicum.event.dto.NewEventDto;
+import ru.practicum.event.dto.UpdateEventUserRequest;
+import ru.practicum.event.service.EventService;
+import ru.practicum.event.service.EventsPrivateGetRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -9,5 +20,44 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/users")
-public class EventPrivateController {}
+@RequestMapping("/users/{userId}/events")
+public class EventPrivateController {
+    private final EventService eventService;
+
+    @GetMapping()
+    public Collection<EventShortDto> getEventsOfUserPaged(
+            @PathVariable Long userId,
+            @RequestParam(defaultValue = "0") int from,
+            @RequestParam(defaultValue = "10") int size) {
+        EventsPrivateGetRequest getRequest = new EventsPrivateGetRequest(userId, from, size);
+        log.info("Private get events requested with params= {}", getRequest);
+        return eventService.getEvents(getRequest);
+    }
+
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
+    public EventFullDto createEvent(
+            @PathVariable Long userId, @RequestBody @Valid NewEventDto newEventDto) {
+        log.info("Create event requested with body= {}", newEventDto);
+        return eventService.createEvent(userId, newEventDto);
+    }
+
+    @GetMapping("/{eventId}")
+    public EventFullDto getEvent(@PathVariable Long userId, @PathVariable Long eventId) {
+        log.info("Private get event requested with userId= {}, eventId= {}", userId, eventId);
+        return eventService.getByUserById(userId, eventId);
+    }
+
+    @PatchMapping("/{eventId}")
+    public EventFullDto updateEvent(
+            @PathVariable Long userId,
+            @PathVariable Long eventId,
+            @RequestBody @Valid UpdateEventUserRequest updateRequest) {
+        log.info(
+                "Private update event requested with userId= {}, eventId= {}, body = {}",
+                userId,
+                eventId,
+                updateRequest);
+        return eventService.updateEventByUserById(userId, eventId, updateRequest);
+    }
+}
