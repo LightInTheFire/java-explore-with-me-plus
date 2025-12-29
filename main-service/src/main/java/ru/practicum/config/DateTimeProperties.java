b64669e3ed24e500dc34e6cb04e3340c4066d7d8
Ilia Egorov <aleilmessia@mail.ru>
2025-12-29 05:44:09 +0300

    feat: add datetime properties, correct LDT requestparam mapping and changed pattern in jsonconfig to properties

---
+++ main-service/src/main/java/ru/practicum/config/DateTimeProperties.java
@@ -0,0 +1,13 @@
+package ru.practicum.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Component
+@ConfigurationProperties(prefix = "app")
+public class DateTimeProperties {
+    @Getter @Setter private String dateTimeFormat = "yyyy-MM-dd HH:mm:ss";
+}
