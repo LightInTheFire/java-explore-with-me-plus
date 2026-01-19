6db76a0809538d31d4eaf2128b9a429946bd1fd1
Ilia Egorov <aleilmessia@mail.ru>
2026-01-19 08:57:35 +0300

    feat: add comment controllers

---
+++ main-service/src/main/java/ru/practicum/comment/controller/CommentAdminController.java
@@ -0,0 +1,27 @@
+package ru.practicum.comment.controller;
+
+import ru.practicum.comment.service.CommentService;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Validated
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("admin/events/{eventId}/comments")
+public class CommentAdminController {
+    private final CommentService commentService;
+
+    @DeleteMapping("/{commentId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteComment(@PathVariable long eventId, @PathVariable long commentId) {
+        log.info("Admin delete comment requested for eventId={}, commentId={}", eventId, commentId);
+
+        commentService.deleteComment(eventId, commentId);
+    }
+}
